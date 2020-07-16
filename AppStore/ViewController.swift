0b05a7b9b77eb59b8b2c023b6c5717e3d42a4f89
krauserhuang <crescentdence@gmail.com>
2020-07-16 12:02:28 +0800

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Tai Chin Huang on 2020/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
